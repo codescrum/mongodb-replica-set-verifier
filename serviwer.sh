137759df5d54589e93f3247e706d43fe30119c67
Johan Tique <johan.tique@codescrum.com>
2014-01-24 14:11:37 -0500

    add new script to launch unicorn and resque services

---
+++ serviwer.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+READY=false
+TOKEN=true
+while : ; do
+    sleep 1
+    if grep -Fxq "$TOKEN" /tmp/token.rsv
+	then
+	    READY=true
+	else
+	    READY=false
+	fi
+	echo $READY
+    ! $READY || break
+done
+echo "" > /tmp/token.rsv
+echo "ready to launch.."
