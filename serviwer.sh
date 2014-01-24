162114ffa8a618fccc50f433563b5d85584bd154
Johan Tique <johan.tique@codescrum.com>
2014-01-24 15:15:18 -0500

    convert serviwer in a service

--- serviwer.sh
+++ serviwer.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
-READY=false
-TOKEN=true
-while : ; do
-    sleep 1
-    if grep -Fxq "$TOKEN" /tmp/token.rsv
-	then
-	    READY=true
-	else
-	    READY=false
-	fi
-	echo $READY
-    ! $READY || break
-done
-echo "" > /tmp/token.rsv
-echo "ready to launch.."
-service unicorn_sardjv start
-service resque start
+
+trigger() {
+	READY=false
+	TOKEN=true
+	while : ; do
+	    sleep 1
+	    if grep -Fxq "$TOKEN" /tmp/token.rsv
+		then
+		    READY=true
+		else
+		    READY=false
+		fi
+		echo $READY
+	    ! $READY || break
+	done
+	echo "$PID"
+	echo "" > /tmp/token.rsv	
+	echo "ready to launch.."
+	# service unicorn_sardjv start
+	# service resque start
+}
+
+trigger & 
+PID=$!
+echo "$PID" > /tmp/serviwer.pid
